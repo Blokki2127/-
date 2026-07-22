6b0c778167befadf33681606692640b2f1f2d046
Blokki <121482218+wdsj2127@users.noreply.github.com>
2026-07-23 00:05:03 +0800

    修改线性基check逻辑

--- 线性基.cpp
+++ 线性基.cpp
@@ -27,6 +27,7 @@ typedef struct linear_basis {
         for (int i = bits_of_type - 1; i >= 0; i--) {
             if (a >> i & 1) {
                 if (!v[i]) return false;
+                a ^= v[i];
             }
         }
         return true;
