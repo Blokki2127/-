216ccd61128d7ffac52be320c453ecc58eafa453
Blokki <Blokki2127@outlook.com>
2026-07-23 08:09:43 +0800

    加入了快速幂板子喵
    快速幂板子
    求 a 的 b 次幂模 p
    (a ^ b) mod p

---
+++ 快速幂.cpp
@@ -0,0 +1,31 @@
+/// 快速幂板子
+/// 求 a 的 b 次幂模 p
+/// (a ^ b) mod p
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+ *  模板开始
+ */
+
+long long fast_exponentiation(long long a, long long b, long long p) {
+    int res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % p;
+        b >>= 1;
+        a = (a * a) % p;
+    }
+    return res;
+}
+
+/*
+ *  模板结束
+ */
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int a, b, p;
+    cin >> a >> b >> p;
+    cout << a << '^' << b << " mod " << p << '=' << fast_exponentiation(a, b, p) << endl;
+}
